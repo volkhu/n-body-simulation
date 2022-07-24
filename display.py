1d1b8c3587002d2de41b6b6553de60fd152c1d12
volkhu <104153914+volkhu@users.noreply.github.com>
2022-07-24 13:14:48 +0300

    Show particles' future trajectories

--- display.py
+++ display.py
@@ -8,7 +8,8 @@ CROSSHAIR_COLOR = (255, 32, 0)
 CROSSHAIR_SIZE = 15
 CROSSHAIR_WIDTH = 1
 PARTICLE_COLOR = (255, 255, 0)
-PARTICLE_TRAIL_COLOR = (64, 64, 64)
+PARTICLE_TRAIL_COLOR = (128, 128, 128)
+PARTICLE_TRAJECTORY_COLOR = (48, 48, 48)
 PARTICLE_TRAIL_WIDTH = 3
 SSAA_FACTOR = 1.5
 
@@ -19,26 +20,27 @@ class Display:
         self.screen = pygame.display.set_mode(screen_size)
         self.set_ssaa_enabled(False)
         self.track_center_of_mass = True
-    
+
     def set_ssaa_enabled(self, enabled):
         self.ssaa_factor = SSAA_FACTOR if enabled else 1.0
         screen_size = self.screen.get_size()
-        surface_size = (screen_size[0] * self.ssaa_factor, screen_size[1] * self.ssaa_factor)
+        surface_size = (screen_size[0] * self.ssaa_factor,
+                        screen_size[1] * self.ssaa_factor)
         self.surface = pygame.Surface(surface_size)
-    
+
     def get_ssaa_enabled(self):
         return self.ssaa_factor > 1.0
-    
+
     def center_camera_on_center_of_mass(self, simulation, camera):
         center_of_screen = self.get_center_of_screen()
         camera.position = simulation.center_of_mass.copy()
         camera.position[0] -= center_of_screen[0] / camera.zoom_factor
         camera.position[1] -= center_of_screen[1] / camera.zoom_factor
-    
+
     def render(self, simulation, camera):
         if self.track_center_of_mass:
             self.center_camera_on_center_of_mass(simulation, camera)
-        
+
         # clear the render surface first with a background color
         self.surface.fill(BACKGROUND_COLOR)
 
@@ -47,19 +49,20 @@ class Display:
         particles_history = simulation.get_particles_history()
 
         for particle_id in particles.keys():
-            self.draw_particle_trail(particle_id, particles_history, camera)
+            self.draw_particle_trail(
+                particle_id, particles, particles_history, camera)
 
         for particle in particles.values():
             self.draw_particle(particle, camera)
-        
+
         if self.track_center_of_mass:
             self.draw_tracking_crosshair()
-        
+
         # copy render surface to the screen and update it
         pygame.transform.smoothscale(
             self.surface, self.screen.get_size(), self.screen)
         pygame.display.update()
-    
+
     def draw_particle(self, particle, camera):
         radius = math.ceil(particle.mass * camera.zoom_factor)
         position_on_screen = self.get_position_on_screen(
@@ -67,12 +70,16 @@ class Display:
 
         self.draw_circle(position_on_screen, radius, PARTICLE_COLOR)
 
-    def draw_particle_trail(self, particle_id, particles_history, camera):
+    def draw_particle_trail(self, particle_id, current_particles, particles_history, camera):
         future_screen_position = None
+        color = PARTICLE_TRAIL_COLOR
 
-        for snapshot in reversed(particles_history):
+        for snapshot in particles_history:
             if particle_id not in snapshot:
-                break  # history doesn't go any further back for this particle
+                break  # the end
+
+            if snapshot == current_particles:
+                color = PARTICLE_TRAJECTORY_COLOR
 
             particle = snapshot[particle_id]
             particle_screen_position = self.get_position_on_screen(
@@ -80,7 +87,7 @@ class Display:
 
             if future_screen_position:
                 self.draw_line(particle_screen_position,
-                               future_screen_position, PARTICLE_TRAIL_WIDTH, PARTICLE_TRAIL_COLOR)
+                               future_screen_position, PARTICLE_TRAIL_WIDTH, color)
 
             future_screen_position = particle_screen_position
 
@@ -96,12 +103,12 @@ class Display:
             draw_end = self.get_position_on_render_surface(end)
             pygame.draw.line(self.surface, color, draw_beginning,
                              draw_end, int(width * self.ssaa_factor))
-    
+
     def draw_tracking_crosshair(self):
         points = []
         for i in range(4):
             points.append(self.get_center_of_screen())
-        
+
         # horizontal line
         points[0][0] -= CROSSHAIR_SIZE
         points[1][0] += CROSSHAIR_SIZE
@@ -110,7 +117,7 @@ class Display:
         points[2][1] -= CROSSHAIR_SIZE
         points[3][1] += CROSSHAIR_SIZE
         self.draw_line(points[2], points[3], CROSSHAIR_WIDTH, CROSSHAIR_COLOR)
-    
+
     def get_position_on_screen(self, world_position, camera):
         position_offset = [world_position[0] - camera.position[0],
                            world_position[1] - camera.position[1]]
@@ -118,33 +125,33 @@ class Display:
 
     def get_position_on_render_surface(self, screen_position):
         return [int(screen_position[0] * self.ssaa_factor), int(screen_position[1] * self.ssaa_factor)]
-    
+
     def is_point_on_screen(self, point):
         screen_size = self.screen.get_size()
-        
+
         if point[0] < 0 or point[0] > screen_size[0]:
-            return False # out of horizontal boundaries
+            return False  # out of horizontal boundaries
         if point[1] < 0 or point[1] > screen_size[1]:
-            return False # out of vertical boundaries
-        
+            return False  # out of vertical boundaries
+
         return True
-    
+
     def is_circle_on_screen(self, position, radius):
         screen_size = self.screen.get_size()
         if position[0] + radius < 0 or position[0] - radius > screen_size[0]:
-            return False # out of horizontal boundaries
+            return False  # out of horizontal boundaries
         elif position[1] + radius < 0 or position[1] - radius > screen_size[1]:
-            return False # out of vertical boundaries
-        
+            return False  # out of vertical boundaries
+
         return True
-    
+
     def get_default_camera_position(self):
         # since the top left corner of the window is the (0, 0) coordinate point,
         # offset the default camera position by half the window size in the opposite
         # direction to center the (0, 0) coordinate point in the window center
         center_of_screen = self.get_center_of_screen()
         return [-center_of_screen[0], -center_of_screen[1]]
-    
+
     def get_center_of_screen(self):
         screen_size = self.screen.get_size()
         return [screen_size[0] / 2, screen_size[1] / 2]
--- models/simulation.py
+++ models/simulation.py
@@ -1,7 +1,7 @@
 from collections import deque
 from copy import deepcopy
 
-PARTICLES_HISTORY_DEPTH = 60
+PARTICLES_HISTORY_DEPTH = 100
 
 
 class Simulation:
@@ -13,6 +13,11 @@ class Simulation:
 
     def add_particle(self, particle):
         current_particles = self.get_current_timestep_particles()
+
+        # invalidate all calculated future timesteps
+        while self.particles_history[-1] != current_particles:
+            self.particles_history.pop()
+
         current_particles[self.particle_id_counter] = particle
         self.particle_id_counter += 1
 
@@ -20,20 +25,24 @@ class Simulation:
         return self.particles_history
 
     def get_current_timestep_particles(self):
-        return self.particles_history[-1]
+        index = int(len(self.particles_history) / 2)
+        return self.particles_history[index]
 
+    # advance the latest frame in the particles history list
     def advance(self, integrator, timestep):
         # make a copy of particles for this new timestep to keep the history intact
-        copy_of_particles = deepcopy(self.get_current_timestep_particles())
+        copy_of_particles = deepcopy(self.particles_history[-1])
         self.particles_history.append(copy_of_particles)
-
-        # only keep a certain amount of past snapshots in memory
-        if len(self.particles_history) > PARTICLES_HISTORY_DEPTH:
-            self.particles_history.popleft()
 
         integrator.integrate(copy_of_particles, timestep)
         self.update_center_of_mass()
-    
+
+        # keep a certain amount of past snapshots in memory
+        if len(self.particles_history) > PARTICLES_HISTORY_DEPTH:
+            self.particles_history.popleft()
+        else:
+            self.advance(integrator, timestep)
+
     def update_center_of_mass(self):
         # calculate a weighted average on each axis
         mass_sum = 0
@@ -42,5 +51,6 @@ class Simulation:
             mass_sum += particle.mass
             position_sum[0] += particle.position[0] * particle.mass
             position_sum[1] += particle.position[1] * particle.mass
-        
-        self.center_of_mass = [position_sum[0] / mass_sum, position_sum[1] / mass_sum]
+
+        self.center_of_mass = [position_sum[0] /
+                               mass_sum, position_sum[1] / mass_sum]
